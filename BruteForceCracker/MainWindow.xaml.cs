6e75b30fcd443448b38da6447d12470b6de9981a
illia.hordiienko@sa.stud.vu.lt <illia.hordiienko@sa.stud.vu.lt>
2026-06-05 00:16:43 +0300

    Stage 2: Completed GUI layout

--- BruteForceCracker/MainWindow.xaml.cs
+++ BruteForceCracker/MainWindow.xaml.cs
@@ -1,24 +1,31 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace BruteForceCracker
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            TxtTargetInfo.Text = "Target generated! (Hash placeholder)";
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            BtnStart.IsEnabled = false;
+            BtnStop.IsEnabled = true;
+            TxtResult.Text = "Searching...";
+        }
+
+        private void BtnStop_Click(object sender, RoutedEventArgs e)
+        {
+            BtnStart.IsEnabled = true;
+            BtnStop.IsEnabled = false;
+            TxtResult.Text = "Stopped.";
         }
     }
 }
